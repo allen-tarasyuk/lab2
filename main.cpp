e1ea5d6d1cdbacba362aa68cdaa0bbb6b8910b17
cree321 <42984414+cree321@users.noreply.github.com>
2022-08-24 16:31:07 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
-using namespace std;
-int hello1Function(string name1); int hello2Function(string name2); int main()
-{
-string name1="your name1"; hello1Function(name1); string name2="your name2"; hello2Function(name2); return 0;
-}
-
+// Allen, Trazler
+// Description: Hello World in C++ 
+//============================================================ 
+main 
+ 
+#include <iostream> 
+using namespace std; 
+int hello1Function(string name1); 
+int hello2Function(string name2); 
+int main() 
+{ 
+ string name1="your name1"; 
+ hello1Function(name1); 
+ string name2="your name2"; 
+ hello2Function(name2); 
+ return 0; 
+} 
